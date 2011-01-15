1da6aa221c93c6674e40f9ae1c60165e6987488f
James Pettit <etherealmachine@gmail.com>
2011-01-14 23:18:28 -0800

    Branching into server (read/write using pipe) and console (r/w using stdin/stdout). Use "server" as first and only argument to enable server mode.

--- imdbot.py
+++ imdbot.py
@@ -3,52 +3,33 @@ import os, sys
 from threading import Thread
 import NLU, Planner, NLG
 
-try:
-	os.remove("/tmp/imdbot_pid")
-except:
-	pass
+server = False
+if sys.argv[1] == 'server':
+	server = True
+	try:
+		os.remove('/tmp/imdbot_pid')
+	except:
+		pass
 	
-pid_file = open("/tmp/imdbot_pid", 'w')
-pid_file.write("{0}\n".format(os.getpid()))
-pid_file.close()
+	pid_file = open('/tmp/imdbot_pid', 'w')
+	pid_file.write("{0}\n".format(os.getpid()))
+	pid_file.close()
 
-# input
-r1, w1 = os.pipe()
-pipe1_r = os.fdopen(r1, 'r')
-pipe1_w = os.fdopen(w1, 'w')
-# output
-r2, w2 = os.pipe()
-pipe2_r = os.fdopen(r2, 'r')
-pipe2_w = os.fdopen(w2, 'w')
-
-class Redirect(Thread):
-   def __init__ (self, r, w, sendId=False):
-   	Thread.__init__(self)
-   	self.setDaemon(True)
-   	self.r = r
-   	self.w = w
-   	self.sendId = sendId
-   def run(self):
-   	while 1:
-   		line = self.r.readline()
-   		if self.sendId:
-   			if line == "":
-   				self.w.write("-1\n")
-   			else:
-   				self.w.write("console\n")
-   		self.w.write(line)
-   		self.w.flush()
-   	
-Redirect(sys.stdin, pipe1_w, sendId=True).start()
-Redirect(pipe2_r, sys.stdout).start()
+	# input
+	r, _ = os.pipe()
+	sys.stdin = os.fdopen(r, 'r')
+	# output
+	_, w = os.pipe()
+	sys.stdout = os.fdopen(w, 'w')
 
 while 1:
-	id = pipe1_r.readline().strip()
-	if id == "-1":
-		break
-	utterance = pipe1_r.readline().strip()
+	if server:
+		id = sys.stdin.readline().strip()
+	else:
+		id = 'console'
+	utterance = sys.stdin.readline().strip()
 	utterance = NLG.get_utterance(id, Planner.get_knowledge_or_question(id, NLU.get_meaning(utterance)))
-	pipe2_w.write(utterance + "\n")
-	pipe2_w.flush()
+	sys.stdout.write(utterance + '\n')
+	sys.stdout.flush()
 	
-os.remove("/tmp/imdbot_pid")
+os.remove('/tmp/imdbot_pid')
