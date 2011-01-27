bd5792b11d9bb947ebad832fdb3344311a7d4688
James Pettit <etherealmachine@gmail.com>
2011-01-27 13:20:06 -0800

    Added stubs for various parts of the understanding process.

--- NLU.py
+++ NLU.py
@@ -1,6 +1,7 @@
 import nltk
 import cPickle
 
+# Saved tagger
 tagger_file = open('tagger.pkl', 'rb')
 tagger = cPickle.load(tagger_file)
 tagger_file.close()
@@ -8,15 +9,40 @@ tagger_file.close()
 contractions = {
 	"'s" : 'is',
 	"n't" : 'not',
-	"'re" : 'are'
+	"'re" : 'are',
+	"'m" : 'am',
 }
 
+# Remove contractions from word
+# return word
 def uncontract(word):
 	if word in contractions:
 		return contractions[word]
 	return word
 
+# Query the database for named entities associated with tagged sentence
+# Return a list of entities
+def entity_recognition(tagged_words):
+	return []
+
+# Classify the dialog act of a sentence
+# Replace named entities with object/class label to form meta-sentence
+# Return act classification, along with any extra info
+def classify_dialog_act(tagged_words, entities):
+	return 'unknown'
+	
 def get_meaning(utterance):
+	# uncontract and tokenize utterance
 	words = [uncontract(word) for word in nltk.word_tokenize(utterance)]
+	# tag words with saved tagger
 	tagged_words = tagger.tag(words)
-	return { 'tagged_words' : tagged_words }
+	# get any recognized entities
+	entities = entity_recognition(tagged_words)
+	# classify the dialog act
+	act = classify_dialog_act(tagged_words, entities)
+	return {
+		'utterance' : utterance,
+		'tagged_words' : tagged_words,
+		'entities' : entities,
+		'act' : act,
+	}
