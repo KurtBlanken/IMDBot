693901aaff5a518142f307453d36399d3fd5f571
unknown <327474@.(none)>
2011-03-06 20:51:25 -0800

    Updated trivia.py and added more trivia types

--- trivia.py
+++ trivia.py
@@ -2,57 +2,108 @@
 import re
 
 '''
-  supported attrs:
-    in, plot, when, role, director, producer
+supported attrs:
+
+in, plot, when, role, char, director, producer, produced, directed, acted, recent, last
     
-  trivia handler, updates the data structer with answers to
-  trivia questions. takes in nlu_to_dm_test_input and then append an answer
-  field to the trivia dictionary.
+trivia handler, updates the data structer with answers to
+trivia questions. takes in nlu_to_dm_test_input and then
+append an answer
+field to the trivia dictionary.
 '''
 import random
+import imdb
+import datetime
+
+dbinfo = eval(open('dbinfo.txt').read())
+user = dbinfo['user']
+pw = dbinfo['passwd']
+db = dbinfo['db']
+host = dbinfo['host']
+URI = 'mysql://{0}:{1}@{2}/{3}'.format(user,pw,host,db)
+imdb_inst = imdb.IMDb('sql', uri = URI)
 
 def handle(data):
-	'''
-	for triv in data['trivia']:
-		triv['answer'] = list()
-		if triv['attr'] != '':
-			triv_type = triv['attr']
-			#'in' questions answers true/false
-			if triv_type == 'in':
-				type1= ''
-				type2= ''
-				p_id= ''
-				m_id= ''
-				if len(triv['entities']) == 2:
-					for entity in triv['entities']:
-						if entity[0]== "person":
-							type1, p_id= entity
-						else:
-							type2, m_id= entity
-				if type1 == 'person' and type2 == 'movie':
-					is_person_in_movie(data, p_id, m_id)
-				else:
-					triv['answer'] = 'How can a {0} be in a {1}'.format(type1, type2)
-      #'when' function asking for production year
-			elif triv_type == 'when':
-				type, id = triv['entity']
-				if type == 'movie':
-					mov = data['data['imdbi']'].get_movie(id)
-					triv['answer'] = mov['production_year']
-				else:
-					triv['answer'] = "I don't know about {0}s".format(type)
-=======
-'''
 	triv = data['trivia']
 	if triv['attr'] != '':
 		triv_type = triv['attr']
-		if triv_type == 'role':
+# returns the last three movies an actor or actress filmed
+# this function is very slow since it retrives a person object with imdbpy
+		if triv_type == 'recent' or triv_type == 'last':
+			type, id = data['entities'][0]
+			if type != 'person':
+				triv['answer'] = "Sorry, please tell me the name a person."
+			else:
+				person = imdb_inst.get_person(id)
+				if 'actress' in person.keys():
+				# The three most recent movies are appended onto a list
+					cur = 0
+					triv['answer'] = list()
+					least = now - person['actress'][0]['year']
+					for movie in person['actress']:
+						cur = cur + 1
+						triv['answer'].append(movie['title'])
+						if cur > 2:
+							break
+				elif 'actor' in person.keys():
+					cur = 0
+					triv['answer'] = list()
+					for movie in person['actor']:
+						cur = cur + 1
+						triv['answer'].append(movie['title'])
+						if cur > 2:
+							break
+		# following types return an integer of how many a 			# person directed, acted, or produced	
+		elif triv_type == 'acted':
+			type, id = data['entities'][0]
+			person = data['imdbi'].get_person(id)
+			if 'actor' in person:
+				triv['answer'] = len(person['actor'])
+			elif 'actress' in person:
+				triv['answer'] = len(person['actress'])
+			else:
+				triv['answer'] = 0
+		elif triv_type == 'directed':
+			type, id = data['entities'][0]
+			person = data['imdbi'].get_person(id)
+			if 'director' in person:
+				triv['answer'] = len(person['director'])
+			else:
+				triv['answer'] = 0
+		elif triv_type == 'produced':
+			type, id = data['entities'][0]
+			person = data['imdbi'].get_person(id)
+			if 'producer' in person:
+				triv['answer'] = len(person['producer'])
+			else:
+				triv['answer'] = 0
+		# returns the role a person was involved with in a movie	
+		elif triv_type == 'role':
+			p_id = ''
+			m_id = ''
+			for type, id in data['entities']:
+				if type == 'person':
+					p_id = id
+				elif type == 'movie':
+					m_id = id
+				if p_id != '' and m_id != '':
+					person = data['imdbi'].get_person(p_id)
+					movie = data['imdbi'].get_movie(m_id)
+					triv['answer'] = "{0} was not related to {1}.".format(person['name'],movie['title'])
+					for cast in movie['cast']:
+						if cast['person_id'] == p_id:
+							if cast['role_type'] == 'actor' or cast['role_type'] == 'actress':
+								triv_type = 'char'
+							else:
+								triv['answer'] = cast['role_type']
+		# returns the character name of an actor/actress in a movie
+		if triv_type == 'char':
 			type1= ''
 			type2= ''
 			p_id= ''
 			m_id= ''
-			if len(triv['entities']) == 2:
-				for entity in triv['entities']:
+			if len(data['entities']) == 2:
+				for entity in data['entities']:
 					if entity[0]== "person":
 						type1, p_id= entity
 					else:
@@ -66,13 +117,14 @@ def handle(data):
 						data['imdbi'].cur.execute('select name from char_name where id = {0}'.format(cast['person_role_id']))
 						name = data['imdbi'].cur.fetchall()
 						if len(name) != 0:
+							triv['answer'].append('played ')
 							triv['answer'].append(name[0][0])
 						if 'note' in cast:
 								triv['answer'].append(cast['note'])
 		elif triv_type == 'producer':
-			who_movie(data, triv_type)
+			triv_movies(data, 'producers')
 		elif triv_type == 'director':
-			who_movie(data, triv_type)
+			triv_movies(data, 'directors')
 		elif triv_type == 'plot':
 			type, m_id = data['entities'][0]
 			if type == 'movie':
@@ -88,13 +140,13 @@ def handle(data):
 			triv_movies(data,'actors')
 #'when' function asking for production year
 		elif triv_type == 'when':
-			type, id = triv['entities'][0]
+			type, id = data['entities'][0]
 			if type == 'movie':
 				mov = data['imdbi'].get_movie(id)
 				triv['answer'] = mov['production_year']
-			#elif type == 'person':
-			#	pers = imdb_triv.get_person(id)
-			#	triv['answer'] = pers['birth date']
+			elif type == 'person':
+				pers = imdb_inst.get_person(id)
+				triv['answer'] = pers['birth date']
 			else:
 				triv['answer'] = "I don't know about {0}s".format(type)
 	else:
@@ -109,52 +161,33 @@ returns true/false if a person did a role in a movie for multiple movies
 def triv_movies(data, role):
 	triv = data['trivia']
 	if len(data['entities']) == 1:
-		type, id = data['entities'][0]
-		if type == 'movie':
-			movie = data['imdbi'].get_movie(id)
-			triv['answer'] = list()
-			for ppl in movie[role]:
-				triv['answer'].append(ppl['name'])
-			if role == 'actors':
-				for ppl in movie['actresses']:
-					triv['answer'].append(ppl['name'])
-
-		else:
-			triv['answer'] = "don't know"
+		who_movie(data, role)
 	elif len(data['entities']) == 2:
 		type1= ''
 		type2= ''
 		p_id= ''
 		m_id= ''
-		for entity in triv['entities']:
+		for entity in data['entities']:
 			if entity[0]== "person":
 				type1, p_id= entity
 			else:
 				type2, m_id= entity
 		is_person_in_movie(data, p_id, m_id, role)
 	elif len(data['entities']) > 2:
-		p_id = 0
-		m_id = 0
+		p_id = ''
+		m_id = ''
 		final_ans = False
 		for type, id in data['entities']:
 			if type == 'person':
 				p_id = id
 			if type == 'movie':
 				m_id = id
-			if p_id != 0 and m_id !=0:
+			if p_id != '' and m_id !='':
 				is_person_in_movie(data, p_id, m_id,role)
 				if triv['answer'] == True:
 					final_ans = True
 		triv['answer'] = final_ans		
 
-
-'''<<<<<<< HEAD
-#helper function to find if an actor/actress was in a movie
-def is_person_in_movie(data, p_id, m_id):
-	data['answer'] = False
-	movie = interface.get_movie(m_id)
-	if 'actors' in movie:
-======='''
 #helper function to find if an actor/actress was in a movie.
 #if a producer produced a movie.
 #if a director directed a movie.
@@ -189,7 +222,7 @@ def who_movie(data, triv_type):
 	#print m_id
 	data['trivia']['answer'] = "I don't know."
 	movie = data['imdbi'].get_movie(m_id)
-	if triv_type == 'director':
+	if triv_type == 'directors':
 		if 'directors' in movie:
 			# directors is a list of dictionaries
 			directors= []
@@ -197,7 +230,7 @@ def who_movie(data, triv_type):
 				answers.append(director['name'])
 			
 			#data['trivia']['answer'] = directors
-	elif triv_type == 'producer':
+	elif triv_type == 'producers':
 		if 'producers' in movie:
 			producers= []
 			for producer in movie['producers']:
