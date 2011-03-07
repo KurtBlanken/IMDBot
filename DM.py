31274b85d67e67307194cc83afc4d6769c24fa7a
KurtBlanken <kurtblanken@gmail.com>
2011-03-06 17:00:48 -0800

    Putting everything together. get_initial_recs() done, working on preferences.py

--- DM.py
+++ DM.py
@@ -1,4 +1,5 @@
 import trivia
+import preferences
 user_prefs = {}
 
 def DM(data):
@@ -6,10 +7,12 @@ def DM(data):
   if data['id'] not in user_prefs:
     user_prefs[data['id']] = { 'pos' : set(), 'neg' : set(), 'prefs' : {}, 'recs' : get_initial_recs()}
   if data['act'] == 'pref':
+    newData = data
     data['pos'].union(user_prefs[data['id']]['pos'])
     data['neg'].union(user_prefs[data['id']]['neg'])
     for key, value in user_prefs[data['id']]['prefs'].items():
       data['prefs'][key] = value
+    preferences.handle(user_prefs, newData)
   elif data['act'] == 'trivia':
     trivia.handle(data)
   
@@ -17,4 +20,16 @@ def get_introduction():
   return "Welcome to IMDBot"
   
 def get_initial_recs():
-	return []
+   top100_in = open('top100.txt', 'r')
+   top100 = []
+   while True:
+      line = top100_in.readline()
+      if line == '':
+         break
+      movie = int(line.split()[0])
+   # change this to appropriate accessor
+      movie = imdbi.get_movie(movie)
+      top100.append((movie, 0))
+
+   top100_in.close()
+   return top100
--- Will-progress.md
+++ Will-progress.md
@@ -1,2 +1,13 @@
 I have finished the difference generator (inside of Get_Fuzzy.py), though the weights can stll be changed around.
 Pickle_Recs.py will run through movies in title_index.txt and output the 5 closest. Currently it runs an n^2 query on the movies, but a better solution is possible.
+
+------ 3/6/2011 -------
+get_initial_recs() added to DM.py
+on line 30: need use appropriate imdb database accessor for get_movie
+current list is 100, if this causes performance issues it should be lowered
+
+preferences.py added
+single entry-point function handle added
+call to preferences added to DM.py line 15 (is this correct?)
+Not sure if access of data from user_prefs is correct
+should handle actor, movie
